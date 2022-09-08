cc1481eebd959b8c1cb50dfc112e5a3b1077deee
Uche Victory <73416190+VickoLee@users.noreply.github.com>
2022-09-08 18:32:06 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 #include <stdio.h>
  
 int main(void)
@@ -7,4 +5,3 @@ int main(void)
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
->>>>>>> 29db410bbb8412f2857a6afea143cf3a5476125b
