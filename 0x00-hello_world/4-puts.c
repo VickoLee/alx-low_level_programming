d97e05bc9f535bd980bc0d64b4936dfe7e3892ef
Uche Victory <73416190+VickoLee@users.noreply.github.com>
2022-09-08 17:35:27 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,6 @@
-# !/bin/bash
+#!/bin/bash
 #include <stdio.h>
-
- /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */ 
+ 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
