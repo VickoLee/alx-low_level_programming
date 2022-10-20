9c8a6d9cace94e509d3d5317b3dbbd28d65860b8
Uche Victory <73416190+VickoLee@users.noreply.github.com>
2022-10-20 19:55:18 +0100

    Add files via upload

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,22 +1,29 @@
 #include "lists.h"
 
 /**
- * list_len - Finds the number of elements in
- *            a linked list_t list.
- * @h: The linked list_t list.
- * @elements: a counter var for size_t (nodes)
+ * list_len - Counts the number of elements in a list
+ * @h: The list head
  *
- * Return: The number of elements in h.
+ * Return: The number of elements in the list
  */
 size_t list_len(const list_t *h)
 {
-	size_t elements = 0;
+	size_t size = 0;
+	list_t *node;
 
-	while (h)
+	if (h == NULL)
+		return (0);
+
+	if (h->next != NULL)
+		size++;
+	else
+		return (1);
+
+	node = h->next;
+	for (; node != NULL; size++, node = node->next)
 	{
-		elements++;
-		h = h->next;
+		continue;
 	}
 
-	return (elements);
+	return (size);
 }
